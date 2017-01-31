2fd43484af093692f61c6f6b80da8ec08fd58d0b
Brandon Him <brh55@drexel.edu>
2017-01-31 13:18:31 -0800

    Fix formatting for help

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,11 +5,7 @@ function help(bot, trigger) {
 }
 
 function prompt(trigger) {
-  return `Hello, ${trigger.displayName} following the examples below to start generating: \n\n
-            - make 5 paragraphs \n\n
-            - generate 4 words \n\n
-            - create 20 sentences \n\n
-            - create 3 paragraphs`
+  return `Hello, ${trigger.personDisplayName} following the examples below to start generating: \n\n- make 5 paragraphs \n\n- generate 4 words \n\n- create 20 sentences \n\n- create 3 paragraphs`
 }
 
 module.exports = help;
